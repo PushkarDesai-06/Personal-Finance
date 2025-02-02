19289ec91de2048d8d0c811a04ac4f85e34737db
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-02-02 23:58:40 +0530

    Built half of it

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flyonui/dist/js/*.js",
+    '../path/to/apexcharts/**/*.js', // Include all relevant JS files from ApexCharts
+    './node_modules/flyonui/dist/js/helper-apexcharts.js', // Include helper JS file with tooltip functions and initialization code
+  ],
+  flyonui: {
+    vendors: true // Enable vendor-specific CSS generation
+  },
+  theme: {
+    extend: {
+      colors: {
+        accent: '#7F6EA0'
+      }
+    },
+  },
+  plugins: [
+    require("flyonui"),
+    require("flyonui/plugin") // Require only if you want to use FlyonUI JS component
+  ],
+}
