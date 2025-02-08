76770fef927d2d0ea83e20240599eb824d9b3601
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-02-08 16:56:21 +0530

    Removed the chatbox to make it suggestions focused

--- src/script.js
+++ src/script.js
@@ -1,6 +1,19 @@
 import { toISOStringWithTimezone } from './isoDateTimeZone.js';
+axios.defaults.baseURL = 'http://localhost:8000'
 
-axios.baseURL = 'http://localhost:8000'
+async function temp() {
+    try {
+        const res = await axios.post('/Suggestions' , {data : JSON.parse(localStorage.getItem('expenses'))})
+        console.log(res.data)
+    }
+    catch (err) {
+        console.log(err)
+
+    }
+}
+
+// temp()
+
 
 
 let DATA = {
@@ -18,8 +31,72 @@ let DATA = {
         { description: "Rent", amount: 1000, date: "2025-02-03", index: 2 },
 
     ],
+    "2025-01-01": [{ description: "January expense", amount: 137, date: "2025-01-01", index: 0 }],
+    "2025-01-02": [{ description: "January expense", amount: 92,  date: "2025-01-02", index: 0 }],
+    "2025-01-03": [{ description: "January expense", amount: 156, date: "2025-01-03", index: 0 }],
+    "2025-01-04": [{ description: "January expense", amount: 68,  date: "2025-01-04", index: 0 }],
+    "2025-01-05": [{ description: "January expense", amount: 180, date: "2025-01-05", index: 0 }],
+    "2025-01-06": [{ description: "January expense", amount: 110, date: "2025-01-06", index: 0 }],
+    "2025-01-07": [{ description: "January expense", amount: 145, date: "2025-01-07", index: 0 }],
+    "2025-01-08": [{ description: "January expense", amount: 75,  date: "2025-01-08", index: 0 }],
+    "2025-01-09": [{ description: "January expense", amount: 167, date: "2025-01-09", index: 0 }],
+    "2025-01-10": [{ description: "January expense", amount: 98,  date: "2025-01-10", index: 0 }],
+    "2025-01-11": [{ description: "January expense", amount: 134, date: "2025-01-11", index: 0 }],
+    "2025-01-12": [{ description: "January expense", amount: 153, date: "2025-01-12", index: 0 }],
+    "2025-01-13": [{ description: "January expense", amount: 60,  date: "2025-01-13", index: 0 }],
+    "2025-01-14": [{ description: "January expense", amount: 120, date: "2025-01-14", index: 0 }],
+    "2025-01-15": [{ description: "January expense", amount: 175, date: "2025-01-15", index: 0 }],
+    "2025-01-16": [{ description: "January expense", amount: 112, date: "2025-01-16", index: 0 }],
+    "2025-01-17": [{ description: "January expense", amount: 133, date: "2025-01-17", index: 0 }],
+    "2025-01-18": [{ description: "January expense", amount: 90,  date: "2025-01-18", index: 0 }],
+    "2025-01-19": [{ description: "January expense", amount: 86,  date: "2025-01-19", index: 0 }],
+    "2025-01-20": [{ description: "January expense", amount: 142, date: "2025-01-20", index: 0 }],
+    "2025-01-21": [{ description: "January expense", amount: 188, date: "2025-01-21", index: 0 }],
+    "2025-01-22": [{ description: "January expense", amount: 97,  date: "2025-01-22", index: 0 }],
+    "2025-01-23": [{ description: "January expense", amount: 128, date: "2025-01-23", index: 0 }],
+    "2025-01-24": [{ description: "January expense", amount: 111, date: "2025-01-24", index: 0 }],
+    "2025-01-25": [{ description: "January expense", amount: 165, date: "2025-01-25", index: 0 }],
+    "2025-01-26": [{ description: "January expense", amount: 150, date: "2025-01-26", index: 0 }],
+    "2025-01-27": [{ description: "January expense", amount: 79,  date: "2025-01-27", index: 0 }],
+    "2025-01-28": [{ description: "January expense", amount: 139, date: "2025-01-28", index: 0 }],
+    "2025-01-29": [{ description: "January expense", amount: 157, date: "2025-01-29", index: 0 }],
+    "2025-01-30": [{ description: "January expense", amount: 103, date: "2025-01-30", index: 0 }],
+    "2025-01-31": [{ description: "January expense", amount: 180, date: "2025-01-31", index: 0 }],
 
+
+    "2025-12-01": [{ description: "December expense", amount: 164, date: "2025-12-01", index: 0 }],
+    "2025-12-02": [{ description: "December expense", amount: 178, date: "2025-12-02", index: 0 }],
+    "2025-12-03": [{ description: "December expense", amount: 112, date: "2025-12-03", index: 0 }],
+    "2025-12-04": [{ description: "December expense", amount: 150, date: "2025-12-04", index: 0 }],
+    "2025-12-05": [{ description: "December expense", amount: 145, date: "2025-12-05", index: 0 }],
+    "2025-12-06": [{ description: "December expense", amount: 129, date: "2025-12-06", index: 0 }],
+    "2025-12-07": [{ description: "December expense", amount: 120, date: "2025-12-07", index: 0 }],
+    "2025-12-08": [{ description: "December expense", amount: 185, date: "2025-12-08", index: 0 }],
+    "2025-12-09": [{ description: "December expense", amount: 171, date: "2025-12-09", index: 0 }],
+    "2025-12-10": [{ description: "December expense", amount: 142, date: "2025-12-10", index: 0 }],
+    "2025-12-11": [{ description: "December expense", amount: 110, date: "2025-12-11", index: 0 }],
+    "2025-12-12": [{ description: "December expense", amount: 198, date: "2025-12-12", index: 0 }],
+    "2025-12-13": [{ description: "December expense", amount: 123, date: "2025-12-13", index: 0 }],
+    "2025-12-14": [{ description: "December expense", amount: 159, date: "2025-12-14", index: 0 }],
+    "2025-12-15": [{ description: "December expense", amount: 167, date: "2025-12-15", index: 0 }],
+    "2025-12-16": [{ description: "December expense", amount: 134, date: "2025-12-16", index: 0 }],
+    "2025-12-17": [{ description: "December expense", amount: 190, date: "2025-12-17", index: 0 }],
+    "2025-12-18": [{ description: "December expense", amount: 115, date: "2025-12-18", index: 0 }],
+    "2025-12-19": [{ description: "December expense", amount: 132, date: "2025-12-19", index: 0 }],
+    "2025-12-20": [{ description: "December expense", amount: 149, date: "2025-12-20", index: 0 }],
+    "2025-12-21": [{ description: "December expense", amount: 155, date: "2025-12-21", index: 0 }],
+    "2025-12-22": [{ description: "December expense", amount: 176, date: "2025-12-22", index: 0 }],
+    "2025-12-23": [{ description: "December expense", amount: 180, date: "2025-12-23", index: 0 }],
+    "2025-12-24": [{ description: "December expense", amount: 101, date: "2025-12-24", index: 0 }],
+    "2025-12-25": [{ description: "December expense", amount: 188, date: "2025-12-25", index: 0 }],
+    "2025-12-26": [{ description: "December expense", amount: 157, date: "2025-12-26", index: 0 }],
+    "2025-12-27": [{ description: "December expense", amount: 142, date: "2025-12-27", index: 0 }],
+    "2025-12-28": [{ description: "December expense", amount: 165, date: "2025-12-28", index: 0 }],
+    "2025-12-29": [{ description: "December expense", amount: 134, date: "2025-12-29", index: 0 }],
+    "2025-12-30": [{ description: "December expense", amount: 160, date: "2025-12-30", index: 0 }],
+    "2025-12-31": [{ description: "December expense", amount: 175, date: "2025-12-31", index: 0 }],
 };
+
 
 // updateLocalStorage();
 
@@ -29,13 +106,12 @@ let totalExpenses = {
 
 const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-
-
 const addExpensesBtn = document.querySelector('#addExpensesFinal');
 const expensesTodayDate = document.querySelector('#dateToday');
 
 let date = new Date()
 let today = toISOStringWithTimezone(date).split('T')[0]
+
 // console.log(today);
 const currentMonth = Months[new Date().getMonth()];
 const lastMonth = Months[new Date().getMonth() - 1];
@@ -72,12 +148,15 @@ function fillTotalExpenses() {
 expensesTodayDate.innerHTML = today;
 // console.log('changed');
 
-// Load data from localStorage
+//! Load data from localStorage
 const expenses = localStorage.getItem('expenses');
+
 if (expenses) {
     DATA = JSON.parse(expenses);
     generateTable();
 }
+
+addDateToData(today);
 
 function updateLocalStorage() {
     localStorage.setItem('expenses', JSON.stringify(DATA));
@@ -218,6 +297,7 @@ expenseForm.addEventListener('submit', function (event) {
     fillTotalExpenses();
     updateLocalStorage();
     updateChart();
+    fetchFromBackend()
     // expenseForm.reset();
 });
 
@@ -330,41 +410,33 @@ function updateChart() {
 
 
 //! START Chatbot //
-
-
-async function sendToBackend(message) {
-    const res = await axios.post('/Suggestions', { message: message, data: JSON.parse(localStorage.getItem('expenses')) })
-
-}
-
 const chatMessages = document.getElementById('chat-messages');
 const chatInput = document.getElementById('chat-input');
-const chatSend = document.getElementById('chat-send');
+
+
+async function fetchFromBackend(message) {
+    try{
+
+        const res = await axios.post('/Suggestions', { data: JSON.parse(localStorage.getItem('expenses')) })
+        const data = res.data.response;
+        console.log(res);
+        addMessage(data, 'bot');
+    }catch(err){
+        console.log(err)
+    }
+        
+}
+
+
 
 function addMessage(text, sender = 'user') {
     const messageEl = document.createElement('div');
     messageEl.className = sender === 'user' ? 'text-right mb-2' : 'text-left mb-2';
-    messageEl.textContent = text;
+    messageEl.classList.add('rounded-md', 'p-2' ,'px-4', 'bg-blue-600' , 'bg-opacity-10' , 'border', 'border-neutral-600' , 'border-opacity-50');
+    messageEl.innerHTML = text;
     chatMessages.appendChild(messageEl);
     chatMessages.scrollTop = chatMessages.scrollHeight;
 }
-
-function botResponse(userText) {
-    // Gemini chatbot simple simulated response.
-    return `Gemini Chatbot: You said "${userText}"`;
-}
-
-chatSend.addEventListener('click', () => {
-    const text = chatInput.value.trim();
-    if (!text) return;
-
-
-});
-
-chatInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') chatSend.click();
-});
-
 
 
 const toggleModal = (id) => {
